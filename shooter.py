3c87c9a794e5b1b7815d355786bdc3c363f19b4e
Dark angel <50552311+darkamgel@users.noreply.github.com>
2021-08-18 09:30:44 +0545

    .

--- shooter.py
+++ shooter.py
@@ -11,8 +11,6 @@ SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pygame.display.set_caption("D-Kill")
 
-
-
 class Soldier(pygame.sprite.Sprite):
     
     def __init__(self,x,y,scale):
