8fb7ff28b2c1b06caab48f28852c5951cda3c678
Dark angel <50552311+darkamgel@users.noreply.github.com>
2021-08-18 09:30:09 +0545

    player Addition

--- shooter.py
+++ shooter.py
@@ -12,10 +12,47 @@ screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pygame.display.set_caption("D-Kill")
 
 
+
+class Soldier(pygame.sprite.Sprite):
+    
+    def __init__(self,x,y,scale):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load('img/player/Idle/0.png')
+        self.image = pygame.transform.scale(img, (int(img.get_width()*scale),int(img.get_height()*scale)))
+        self.rect = self.image.get_rect()
+        self.rect.center = (x,y)
+    
+    def draw(self):
+        screen.blit(self.image,self.rect)
+        
+        
+        
+        
+
+player = Soldier(200,200,3)
+player2 = Soldier(400,200,3)
+
+
+
+
+
+
 run = True
 while run:
+    
+    player.draw()
+    
+    
+    
+    
+    
+    
+    
     for event in pygame.event.get():
         # quit game
         if event.type == pygame.QUIT:
             run = False
+    pygame.display.update()
+            
+            
 pygame.quit()
