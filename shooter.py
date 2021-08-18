890c89b05575eb139932d0ac03c195883be64200
Dark angel <50552311+darkamgel@users.noreply.github.com>
2021-08-18 09:03:25 +0545

    Pygame window creation

--- shooter.py
+++ shooter.py
@@ -9,3 +9,13 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+pygame.display.set_caption("D-Kill")
+
+
+run = True
+while run:
+    for event in pygame.event.get():
+        # quit game
+        if event.type == pygame.QUIT:
+            run = False
+pygame.quit()
